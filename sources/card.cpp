ecbd6764335f21cb17d482f5501e3effb8a3ccab
tristangory <tristan.gory@epitech.eu>
2022-08-06 18:39:21 +0200

    [ADD] Added card.cpp it only contains the constructor of the card class

---
+++ sources/card.cpp
@@ -0,0 +1,14 @@
+/*
+** TRISTAN GORY PROJECT, 2022
+** la-bataille
+** File description:
+** card
+*/
+
+#include "card.hpp"
+
+card::card(int color, int power)
+{
+    this->color = color;
+    this->power = power;
+}
